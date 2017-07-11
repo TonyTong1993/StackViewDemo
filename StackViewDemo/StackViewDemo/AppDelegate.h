48e27ad790f6c58bed2a1f09f5b45b17aa19b1ba
tonytong <tongwanhua1993@163.com>
2017-07-11 11:54:20 +0800

    init project

---
+++ StackViewDemo/StackViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StackViewDemo
+//
+//  Created by 童万华 on 2017/7/11.
+//  Copyright © 2017年 童万华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StackViewDemo/StackViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StackViewDemo
+//
+//  Created by 童万华 on 2017/7/11.
+//  Copyright © 2017年 童万华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
